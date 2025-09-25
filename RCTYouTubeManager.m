30dadc92883f6d56354c56331590ebc603a897ce
Justin Pritchard <jpritchard9518@gmail.com>
2025-09-25 12:53:06 -0600

    Potential Fix For UnMute Error

--- RCTYouTubeManager.m
+++ RCTYouTubeManager.m
@@ -40,16 +40,12 @@ RCT_EXPORT_METHOD(seekTo:(nonnull NSNumber *)reactTag seconds:(nonnull NSNumber 
         [youtube seekToSeconds:seconds.floatValue allowSeekAhead:YES];
     }];
 }
-
 RCT_EXPORT_METHOD(mute:(nonnull NSNumber *)reactTag)
 {
     [self.bridge.uiManager addUIBlock:^(__unused RCTUIManager *uiManager, NSDictionary<NSNumber *, UIView *> *viewRegistry) {
         RCTYouTube *youtube = (RCTYouTube*)viewRegistry[reactTag];
-        if ([youtube isKindOfClass:[RCTYouTube class]]) {
-            [youtube mute];
-        } else {
-            RCTLogError(@"Cannot mute: %@ (tag #%@) is not RCTYouTube", youtube, reactTag);
-        }
+
+        [youtube mute];
     }];
 }
 
@@ -57,11 +53,8 @@ RCT_EXPORT_METHOD(unMute:(nonnull NSNumber *)reactTag)
 {
     [self.bridge.uiManager addUIBlock:^(__unused RCTUIManager *uiManager, NSDictionary<NSNumber *, UIView *> *viewRegistry) {
         RCTYouTube *youtube = (RCTYouTube*)viewRegistry[reactTag];
-        if ([youtube isKindOfClass:[RCTYouTube class]]) {
-            [youtube unMute];
-        } else {
-            RCTLogError(@"Cannot unMute: %@ (tag #%@) is not RCTYouTube", youtube, reactTag);
-        }
+        
+        [youtube unMute];
     }];
 }
 
